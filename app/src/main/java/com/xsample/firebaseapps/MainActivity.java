4275028bf3b965bdcdd4de45a8ec77a2314c0fce
knight <tocaldera6734@gmail.com>
2019-01-17 12:13:32 +0530

    Added login layout with username password Added some validations for username and passoword
    validations -
     1. email field is empty
     2. Password is empty
     3. email is in valid format as a email address

--- app/src/main/java/com/xsample/firebaseapps/MainActivity.java
+++ app/src/main/java/com/xsample/firebaseapps/MainActivity.java
@@ -1,13 +1,47 @@
 package com.xsample.firebaseapps;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    TextInputEditText email,password;
+    String emailSt,passwordSt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.EmailAddress);
+        password = findViewById(R.id.Password);
+
+        emailSt = Objects.requireNonNull(email.getText()).toString().trim();
+        passwordSt = Objects.requireNonNull(password.getText()).toString().trim();
+
+
+    }
+
+    public void signUp(View view) {
+        if(emailSt.isEmpty()){
+            email.setError("Please enter a email address");
+            email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailSt).matches()){
+            email.setError("Please enter a valid email address");
+            email.requestFocus();
+            return;
+        }
+        if(passwordSt.isEmpty()){
+            password.setError("Please enter a password");
+            password.requestFocus();
+            return;
+        }
+
     }
 }
